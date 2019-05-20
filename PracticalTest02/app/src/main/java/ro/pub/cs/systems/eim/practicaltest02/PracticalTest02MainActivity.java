2818fc73fd25682c6d79837096ccc57221bc57c5
Root USO User <student@stud.acs.upb.ro>
2019-05-20 10:49:49 +0300

    ServeThread

--- PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
@@ -2,6 +2,7 @@ package ro.pub.cs.systems.eim.practicaltest02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,17 +16,46 @@ public class PracticalTest02MainActivity extends AppCompatActivity {
     Button serverConnect;
     Button clientConnect;
 
+    View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.button: {
+                    try {
+                        Integer port = Integer.parseInt(serverPort.getText().toString());
+                        String prefixValue = prefix.getText().toString();
+                        ServerThread serverThread = new ServerThread(port, prefixValue);
+                        if (serverThread.getServerSocket() == null) {
+                            throw new Exception();
+                        }
+                        serverThread.start();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+                case R.id.button2: {
+
+                    break;
+                }
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test02_main);
 
         serverPort = findViewById(R.id.editText);
-        clientAddress = findViewById(R.id.editText);
-        clientPort = findViewById(R.id.editText);
-        prefix = findViewById(R.id.editText);
+        clientAddress = findViewById(R.id.editText4);
+        clientPort = findViewById(R.id.editText3);
+        prefix = findViewById(R.id.editText2);
 
         serverConnect = findViewById(R.id.button);
         clientConnect = findViewById(R.id.button2);
+
+        serverConnect.setOnClickListener(clickListener);
+        clientConnect.setOnClickListener(clickListener);
     }
 }
